3912ff9caaadd384f3633a74d529e230b9249d47
aalicav <antonioalissoncavalcantedeoliv@gmail.com>
2021-08-16 21:41:16 -0300

    ex002.py

---
+++ ex002.py
@@ -0,0 +1,4 @@
+name = input("Insira seu nome:")
+print('Prazer em conhecê-lo,{}!'.format(name))
+dia = input('Agora tome este drink e me conte como foi o seu dia:')
+print('Até a próxima, meu rei')
---
+++ ex003.py
@@ -0,0 +1,3 @@
+n1 = float(input('Digite o primeiro numero: '))
+n2 = float(input('Digite o segundo numero :'))
+print('A soma entre {} e {} é {}'.format(n1,n2,(n1+n2)))
