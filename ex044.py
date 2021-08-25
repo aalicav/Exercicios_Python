fce8600ebd90bf3b2c0208372b4920a57103c555
aalicav <89048561+aalicav@users.noreply.github.com>
2021-08-25 20:40:00 -0300

    Update ex044.py

--- ex044.py
+++ ex044.py
@@ -1,3 +1,17 @@
 print('''Você pode comprar: 
-1.parcelado
-2.''')
+1.à vista dinheiro ou cheque
+2.à vista no cartão
+3.2x no cartão
+4.3x no cartão''')
+op = int(input('Selecione a forma de pagamento:'))
+custo = float(input('Quanto custa seu produto? (R$)'))
+if op == 1:
+  print('O preço do seu produto é {} R$'.format(0.9*preço))
+elif op == 2:
+  print('O preço do seu produto é {}'.format(0.95*preço))
+elif op == 3:
+  print('O seu produto não recebe desconto')
+elif op == 4: 
+  print('O preço do seu produto é {}'.format(1.2*preço))
+else:
+  print('ERRO! digite um valor da tabela')
